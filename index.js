c277439380ac49ea40492ae8f6237c418b8ea5f9
MxKay <85835225+Wizardz-lab@users.noreply.github.com>
2021-10-29 12:58:53 -0700

    Added index.js + command handler

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const client = new Discord.Client({intents: [
     "GUILD_MESSAGES"
 ]});
 client.on('ready', () => {
-    console.log("The bot is online!")
+    console.log(`${client.user.username} is online!`)
 });
 
 const commands = new Discord.Collection();
